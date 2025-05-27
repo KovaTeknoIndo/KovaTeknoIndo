61214f00dea9fb19aac7562718b629d0e83f2bd3
gonemaul <mazm.021.021@gmail.com>
2025-05-27 11:27:23 +0700

    fix seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,53 @@
 import "./globals.css";
-import { DefaultSeo } from "next-seo";
-import SEO from "../next-seo.config";
-import Head from "next/head";
+import Script from "next/script";
 import Navbar from "./components/Navbar/index";
 import Footer from "./components/Footer/index";
 
 export const metadata = {
-  title: "Kova Teknoindo | Software Agency in Kediri",
-  description: "Kova Teknoindo adalah software agency di Kediri yang menyediakan layanan pembuatan website, aplikasi mobile, dan sistem informasi terbaik untuk bisnis Anda.",
-  keywords: ['jasa website', 'buat website murah', 'web developer UMKM'],
+  title: {
+    default: "Kova Teknoindo | Software Agency in Kediri",
+    template: "%s | Kova Teknoindo",
+  },
+  description:
+    "Kova Teknoindo adalah software agency di Kediri yang menyediakan layanan pembuatan website, aplikasi mobile, dan sistem informasi terbaik untuk bisnis Anda.",
+  metadataBase: new URL("https://www.kovatekno.my.id"),
   openGraph: {
     title: "Kova Teknoindo | Software Agency in Kediri",
-    description: 'Solusi pembuatan website profesional untuk bisnis Anda.',
-    url: 'https://www.kovatekno.my.id',
-    siteName: 'NamaUsaha',
+    description: "Solusi pembuatan website profesional untuk bisnis Anda.",
+    type: "website",
+    locale: "id_ID",
+    url: "https://www.kovatekno.my.id",
+    siteName: "Kova Teknoindo",
     images: [
       {
-        url: 'https://www.kovatekno.my.id/og-image.svg',
+        url: "https://www.kovatekno.my.id/og-image.svg",
         width: 1200,
         height: 630,
-        alt: 'Jasa Website',
+        alt: "Kova Teknoindo",
       },
     ],
-    type: 'website',
+  },
+  // Jika ingin menambahkan Twitter Card:
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "Kova Teknoindo | Software Agency in Kediri",
+  //   description: "Solusi pembuatan website profesional untuk bisnis Anda.",
+  //   creator: "@kova_teknoindo",
+  //   images: ["https://www.kovatekno.my.id/og-image.svg"],
+  // },
+  keywords: [
+    "jasa website",
+    "buat website murah",
+    "web developer UMKM",
+    "jasa pembuatan aplikasi",
+    "software agency Kediri",
+  ],
+  authors: [{ name: "Kova Teknoindo" }],
+  viewport: "width=device-width, initial-scale=1.0",
+  other: {
+    "x-ua-compatible": "IE=edge",
   },
 };
-
 
 export default function RootLayout({
   children,
@@ -34,13 +56,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        {/* Google Analytics */}
-        <script
-          async
+      <body>
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-505RVXTE04"
+          strategy="afterInteractive"
         />
-        <script
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
@@ -50,10 +73,7 @@ export default function RootLayout({
             `,
           }}
         />
-      </Head>
-      <body>
         <Navbar />
-        <DefaultSeo {...SEO} />
         {children}
         <Footer />
       </body>
